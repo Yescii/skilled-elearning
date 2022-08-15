7fdc21914a3547b2a436ed0fe22f6a7409c8123d
Jesse Gonzalez <cgonzalez.jesse@gmail.com>
2022-08-15 19:39:25 -0400

    javascript file named course that export an object

---
+++ src/Courses.js
@@ -0,0 +1,22 @@
+export const Courses = [
+  {
+    title: "Animation",
+    desc: " Learn the latest animation techniques to create stunning motion design and captivate your audience. ",
+  },
+  {
+    title: "Design",
+    desc: "Create beautiful, usable interfaces to help shape the future of how the web looks.",
+  },
+  {
+    title: "Photography",
+    desc: " Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.",
+  },
+  {
+    title: "Crypto",
+    desc: "All you need to know to get started investing in crypto. Go from beginner to advanced with this54 hour course.",
+  },
+  {
+    title: "Business",
+    desc: "A step-by-step playbook to help you start, scale, and sustain your business without outside investment.",
+  },
+];
