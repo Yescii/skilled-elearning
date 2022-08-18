587983611679a9df194444ccb8275cd47aeda40d
Jesse Gonzalez <cgonzalez.jesse@gmail.com>
2022-08-18 00:53:09 -0400

    cleaned up app js file and added div.site element.selector

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="site"></div>
+    </div>
+  );
 }
 
 export default App;
