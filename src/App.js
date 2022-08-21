ee9d5147bec16fed9c4e6f4f32ff4ad6be114d16
Jesse Gonzalez <cgonzalez.jesse@gmail.com>
2022-08-21 01:26:32 -0400

    added a header section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="site"></div>
+      <div className="site">
+        <section className="header">
+          <p>skilled</p>
+          <button>get started</button>
+        </section>
+      </div>
     </div>
   );
 }
