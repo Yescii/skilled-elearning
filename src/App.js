ffeaa31bc905ab830a2d1d3ff3104aa5c36fe131
Jesse Gonzalez <cgonzalez.jesse@gmail.com>
2022-08-25 00:42:35 -0400

    created a hero component and added to the app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <div className="site">
         <Header />
+        <Hero />
       </div>
     </div>
   );
